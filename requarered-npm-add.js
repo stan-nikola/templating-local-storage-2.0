2661a358e90b04cf5f85db1e1317ac754267a4f9
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-10-05 17:09:41 +0300

    fix parcel

--- requarered-npm-add.js
+++
@@ -1,7 +0,0 @@
-// const Joi = require('joi');
-
-// const passwordSchema = Joi.string().alphanum().min(3).max(10).alphanum();
-// console.log('passwordSchema', passwordSchema.validate('fbfdb##'));
-
-const shortid = require('shortid');
-console.log('shortid', shortid.generate());
